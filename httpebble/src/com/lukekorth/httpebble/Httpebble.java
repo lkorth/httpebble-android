c7ffd2e6d3d27bef76f75d0f61088dfb9b42faa8
lkorth <luke@lukekorth.com>
2013-05-18 15:51:11 -0500

    Add user interface

---
+++ httpebble/src/com/lukekorth/httpebble/Httpebble.java
@@ -0,0 +1,13 @@
+package com.lukekorth.httpebble;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Httpebble extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
