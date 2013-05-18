dd5072f980052b32f39ef37c8811cd4397f313ff
lkorth <luke@lukekorth.com>
2013-05-18 00:39:03 -0500

    Add check for possible requests

--- httpebble/src/com/lukekorth/httpebble/receivers/PebbleDataReceiver.java
+++ httpebble/src/com/lukekorth/httpebble/receivers/PebbleDataReceiver.java
@@ -13,6 +13,7 @@ import android.os.PowerManager.WakeLock;
 
 import com.getpebble.android.kit.PebbleKit;
 import com.getpebble.android.kit.util.PebbleDictionary;
+import com.lukekorth.httpebble.Constants;
 
 public class PebbleDataReceiver extends IntentService {
 
@@ -24,24 +25,57 @@ public class PebbleDataReceiver extends IntentService {
 	protected void onHandleIntent(Intent intent) {
 		int transactionId = intent.getIntExtra(TRANSACTION_ID, -1);
 		PebbleKit.sendAckToPebble(this, transactionId);
-		
-        String data = intent.getStringExtra(MSG_DATA);
-        if (data != null && data.length() != 0) {
+
+		String data = intent.getStringExtra(MSG_DATA);
+		if (data != null && data.length() != 0) {
 			PowerManager mgr = (PowerManager) getSystemService(Context.POWER_SERVICE);
 			WakeLock wakeLock = mgr.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
 					"PebbleDataReceiver");
 			wakeLock.acquire();
 
-        	try {
-                PebbleDictionary pebbleDictionary = PebbleDictionary.fromJson(data);
+			try {
+				PebbleDictionary pebbleDictionary = PebbleDictionary.fromJson(data);
 
-                //process data here            
-            } catch (JSONException e) {
-                // silently fail
-            }
+				// http request
+				if (pebbleDictionary.getString(Constants.HTTP_URL_KEY) != null) {
+
+				}
+				// timezone infomation
+				else if (pebbleDictionary
+						.getUnsignedInteger(Constants.HTTP_TIME_KEY) != null) {
+
+				}
+				// location information
+				else if (pebbleDictionary
+						.getUnsignedInteger(Constants.HTTP_LOCATION_KEY) != null) {
+
+				}
+				// setting entries in key-value store
+				else if (pebbleDictionary
+						.getInteger(Constants.HTTP_COOKIE_STORE_KEY) != null) {
+
+				}
+				// removing entries from key-value store
+				else if (pebbleDictionary
+						.getInteger(Constants.HTTP_COOKIE_LOAD_KEY) != null) {
+
+				}
+				// deleting entries from key-value store
+				else if (pebbleDictionary
+						.getInteger(Constants.HTTP_COOKIE_DELETE_KEY) != null) {
+
+				}
+				// fsync
+				else if (pebbleDictionary
+						.getUnsignedInteger(Constants.HTTP_COOKIE_LOAD_KEY) != null) {
+
+				}
+			} catch (JSONException e) {
+				// silently fail
+			}
 
 			wakeLock.release();
-        }
+		}
 	}
 
 }
