2844753a11f76da325f00b9f0c92ed281b235572
lkorth <luke@lukekorth.com>
2013-05-20 00:44:47 -0500

    Add check for Google Play services

--- httpebble/src/com/lukekorth/httpebble/Httpebble.java
+++ httpebble/src/com/lukekorth/httpebble/Httpebble.java
@@ -1,13 +1,66 @@
 package com.lukekorth.httpebble;
 
-import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 
-public class Httpebble extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
+public class Httpebble extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+	}
+
+	@Override
+	protected void onResume() {
+		// Check that Google Play services is available
+		int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+		// If Google Play services is unavailable
+		if (ConnectionResult.SUCCESS != resultCode) {
+			// Get the error dialog from Google Play services
+			Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(resultCode, this, 0);
+
+			// If Google Play services can provide an error dialog
+			if (errorDialog != null) {
+				// Create a new DialogFragment for the error dialog
+				ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+
+				// Set the dialog in the DialogFragment
+				errorFragment.setDialog(errorDialog);
+
+				// Show the error dialog in the DialogFragment
+				errorFragment.show(getSupportFragmentManager(), "Location Updates");
+			}
+		}
+	}
+
+	// Define a DialogFragment that displays the error dialog
+	public static class ErrorDialogFragment extends DialogFragment {
+
+		// Global field to contain the error dialog
+		private Dialog mDialog;
+
+		// Default constructor. Sets the dialog field to null
+		public ErrorDialogFragment() {
+			super();
+			mDialog = null;
+		}
+
+		// Set the dialog to display
+		public void setDialog(Dialog dialog) {
+			mDialog = dialog;
+		}
+
+		// Return a Dialog to the DialogFragment.
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			return mDialog;
+		}
 	}
 }
