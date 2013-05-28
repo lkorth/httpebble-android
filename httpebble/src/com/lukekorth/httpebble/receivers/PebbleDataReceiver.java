401d6bf1e5c7897b2a0871669b62d05e0d9b03f0
lkorth <luke@lukekorth.com>
2013-05-28 18:18:18 -0500

    Fix case sensitivity of UUID check

--- httpebble/src/com/lukekorth/httpebble/receivers/PebbleDataReceiver.java
+++ httpebble/src/com/lukekorth/httpebble/receivers/PebbleDataReceiver.java
@@ -3,9 +3,13 @@ package com.lukekorth.httpebble.receivers;
 import static com.getpebble.android.kit.Constants.APP_UUID;
 import static com.getpebble.android.kit.Constants.MSG_DATA;
 import static com.getpebble.android.kit.Constants.TRANSACTION_ID;
+
+import java.util.UUID;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import com.getpebble.android.kit.PebbleKit;
 import com.lukekorth.httpebble.Constants;
@@ -14,7 +18,11 @@ public class PebbleDataReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		if (((String) intent.getSerializableExtra(APP_UUID)).startsWith(Constants.HTTPEBBLE_UUID_PREFIX)) {
+		String uuid = ((UUID) intent.getSerializableExtra(APP_UUID)).toString().toUpperCase();
+
+		Log.d(Constants.HTTPEBBLE, "Pebble Request UUID: " + uuid);
+
+		if (uuid.startsWith(Constants.HTTPEBBLE_UUID_PREFIX)) {
 			PebbleKit.sendAckToPebble(context, intent.getIntExtra(TRANSACTION_ID, -1));
 
 			String data = intent.getStringExtra(MSG_DATA);
