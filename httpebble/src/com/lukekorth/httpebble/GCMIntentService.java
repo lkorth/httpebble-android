280f31a278c9cd30f747fa53eb6cf09687026624
lkorth <luke@lukekorth.com>
2013-06-02 20:35:35 -0500

    Implement Pebble notification

--- httpebble/src/com/lukekorth/httpebble/GCMIntentService.java
+++ httpebble/src/com/lukekorth/httpebble/GCMIntentService.java
@@ -1,9 +1,16 @@
 package com.lukekorth.httpebble;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.util.Log;
 
 import com.google.android.gcm.GCMBaseIntentService;
 
@@ -17,8 +24,22 @@ public class GCMIntentService extends GCMBaseIntentService {
 
 	@Override
 	protected void onMessage(Context context, Intent intent) {
-		// TODO Auto-generated method stub
+		if (intent.getStringExtra("type").equals("notification")) {
+			final Map<String, Object> data = new HashMap<String, Object>();
+			data.put("title", intent.getStringExtra("title"));
+			data.put("body", intent.getStringExtra("body"));
+			final JSONObject jsonData = new JSONObject(data);
+			final String notificationData = new JSONArray().put(jsonData).toString();
 
+			final Intent i = new Intent("com.getpebble.action.SEND_NOTIFICATION");
+			i.putExtra("messageType", "PEBBLE_ALERT");
+			i.putExtra("sender", Constants.HTTPEBBLE);
+			i.putExtra("notificationData", notificationData);
+
+			Log.d(Constants.HTTPEBBLE, "Notification sent to Pebble: " + notificationData);
+
+			context.sendBroadcast(i);
+		}
 	}
 
 	@Override
