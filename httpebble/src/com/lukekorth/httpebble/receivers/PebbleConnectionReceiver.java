e37a408948a005617278a132ce4d4ab3d8b52c75
lkorth <luke@lukekorth.com>
2013-06-22 23:46:01 -0500

    Add additional connection and disconnection logging

--- httpebble/src/com/lukekorth/httpebble/receivers/PebbleConnectionReceiver.java
+++ httpebble/src/com/lukekorth/httpebble/receivers/PebbleConnectionReceiver.java
@@ -6,13 +6,22 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.lukekorth.httpebble.Constants;
 
 public class PebbleConnectionReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		SharedPreferences sharedPrefs = context.getSharedPreferences(HTTPEBBLE, 0);
-		sharedPrefs.edit().putString(PEBBLE_ADDRESS, intent.getStringExtra("address")).commit();
+		if (intent.getAction().equals("com.getpebble.action.PEBBLE_CONNECTED")) {
+			SharedPreferences sharedPrefs = context.getSharedPreferences(HTTPEBBLE, 0);
+			sharedPrefs.edit().putString(PEBBLE_ADDRESS, intent.getStringExtra("address")).commit();
+
+			Log.d(Constants.HTTPEBBLE, "Pebble " + intent.getStringExtra("address") + " connected");
+		} else if (intent.getAction().equals("com.getpebble.action.PEBBLE_DISCONNECTED")) {
+			Log.d(Constants.HTTPEBBLE, "Pebble " + intent.getStringExtra("address") + " disconnected");
+		}
 	}
 
 }
